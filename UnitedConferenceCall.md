c577ed2d6a8013cfc0db7b325e1380f34b807d0e
Jerad Acosta <JrD@irHacker.com>
2015-05-28 08:41:49 -0700

    added new ggvis graphs and removed stats graphs

---
+++ UnitedConferenceCall.md
@@ -0,0 +1,61 @@
+## United Way  Call
+
+working with Charter Schools
+First grade reading
+Readying for Kindergarten
+
+Currently have a partner mapping:
+attendance
+reading level
+family stability
+
+## Client Needs
++ Show what things are working
++ Create a Dashboard
+
+__Want to create a common set of metrics to measure:__
++ What was our Goal
++ What is on track / not on track
+
+
+### Questions
+How Many Schools are you working with?
+First Grade reading?
+
+
+### For Us
+
+Mission Edge is losing a lot of tech ability
++ People weren't happy with the academic results
+
+Harder and Company
++ Evaluation Focused
++ Not Exploratory Focused
++ Jennifer James (runs the company and has a daughter who goes to preschool with Andrea)
+
+Andrea Knows the Director of United Way
+
+
+August is the likely time line for United Way
+
+## Next Step
+Talk with Hunger Coalition
++ Who is Hungry
++ When and Where
+
++ Where Does money come from
++ How does it get used
+
+Would Take place in August
+
+#### Monarch
+Collecting Data on Reading Program
+Already paying for _future_ system that measures attendance and other variables
+
+
+### Education Aliance
+
+Would take place in Aug /  September
+
+### Just in Time for foster youth [aka Just in Time]
+Would take place in attendance
--- monarch-app/app.R
+++ monarch-app/app.R
@@ -9,6 +9,7 @@ library(shinydashboard)
 # Graphics
 library(ggvis)
 library(ggplot2)
+library(rCharts)
 
 # data manipulation
 library(dplyr)
@@ -22,8 +23,9 @@ eventTable <- table(data$student_id)
 # create data frame from table
 tframe <- as.data.frame(eventTable)
 names(tframe) <- c("id", "freq")
-plot(tframe[[2]], type = 'h', xlab = "Student ID", ylab = " Number of Offenses")
-quantile(eventTable)
+## How It was plotted in data science competition
+# plot(tframe[[2]], type = 'h', xlab = "Student ID", ylab = " Number of Offenses")
+
 tframe <- arrange(tframe, desc(freq))
 
 header <- dashboardHeader(
@@ -43,18 +45,26 @@ header <- dashboardHeader(
                              from = "Support",
                              message = "Contact the Developer.",
                              icon = icon("life-ring")
+                     ),
+                     messageItem(
+                             from = "Example Data Input",
+                             message = c("Click here to see a potential data input form",a(href = "https://40.media.tumblr.com/3d01ef923a34a3546133a12833e5aae2/tumblr_np0zi0MOM31uwjn2uo1_540.png")),
+                             icon = icon("edit")
                      )
         ))
 
 sidebar  <- dashboardSidebar(
+                menuItem("X-Axis", icon = icon("line-chart")),
+        selectInput("yVariable", "Select Variable to Count:",
+                    c("Student ID" = "student_id",
+                      "Grade" = "grade"),selected = "student_id"),
         radioButtons("xaxis", label = ("X-Axis"),
                      choices = list("Student ID" = "student_id",
                                     "Location" = "environment",
                                     "Time" = "date_time",
-                                    "Student Grade" = "grade")),
-        sidebarMenu(
-                menuItem("Y-Axis", tabName = "Yaxis", icon = icon("line-chart"))),
-        menuSubItem("Y-Axis", tabName = "subAxis", icon = icon("line-chart")),
+                                    "Student Grade" = "grade"),
+                     selected = "~student_id"),
+        menuItem(text = "Y-Axis", tabName = "Time", icon = icon("line-chart")),
         radioButtons("yaxis", label = ("Y-Axis"),
                      choices = list("Student ID" = "student_id",
                                     "Location" = "environment",
@@ -62,74 +72,124 @@ sidebar  <- dashboardSidebar(
                                     "Student Grade" = "grade")),
         checkboxInput("conditionalPanel", "Count"),
         conditionalPanel(
-                condition = "input.conditionalPanel == true",
+                condition = "input.conditionalPanel != true",
                 selectInput("conditionalYAxis", "Y-Axis",
-                            list("Student ID" = "student_id",
+                            list("Teacher ID" = "documenting_staff_id",
                                  "Location" = "environment",
                                  "Time" = "date_time",
                                  "Student Grade" = "grade"))
-        )
-        )
+        ),
+        selectInput(inputId = "x",
+                    label = "Choose X",
+                    choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
+                    selected = "SepalLength"),
+        selectInput(inputId = "y",
+                    label = "Choose Y",
+                    choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
+                    selected = "SepalWidth")
+)
 
 body  <- dashboardBody(
         # fluid rows have a width of 12 the entire page
-        fluidRow(12,
-        # tabBox will be used to select the form of visualization
-                # tabBox args: 
-                ## title (displayed on UI), id (for use by server input$id)
-                ## height & width (similar to box arguments), side (which side the tabs are on)
-        tabsetPanel("Plot Type",
-        # Histogram of Referrals per student
-                tabPanel("Count", 
-                        column(12,
-                               plotOutput("plot1", height = "500px"),
-                               radioButtons("referralCount", label = "Arrange By:",
-                                            choices = list("Student ID" = "id", "Frequency" = "freq"),
-                                            selected = "id"
-                                        )
-                                )
-                        ),
-                tabPanel("ggCount", 
-                         column(12,
-                                ggvisOutput("ggvis1"),
-                                radioButtons("ggreferralCount", label = "Arrange By:",
-                                             choices = list("Student ID" = "id", "Frequency" = "freq"),
-                                             selected = "id"
-                                )
-                         )
-                ),
-        # Histogram of Referrals per grade
-                tabPanel("Grade",
-                         column(12,
-#                                  radioButtons("fill", "Select Fill Variable:",
-#                                               choices = list("Environment" = "environment")),
-                                ggvisOutput("plotGrade")
-                                )
-                        ),
-        # histogram of Referrals per hour AND per month
-                tabPanel("Time",
-                         column(12,
-                                plotOutput("time", height = "500px"),
-                                radioButtons("timeType", label = "",
-                                             choices = list("Hours of the Day" = "H",
-                                                            "Day of the Week" = "A",
-                                                            "Month of the Year" = "m")
+        fluidRow("Visualization",
+                 # tabBox will be used to select the form of visualization
+                 # tabBox args: 
+                 ## title (displayed on UI), id (for use by server input$id)
+                 ## height & width (similar to box arguments), side (which side the tabs are on)
+                 tabsetPanel("Plot Type",
+                             tabPanel("Interactive Count",
+                                      column(12,
+                                             ggvisOutput("ggvisPlot"),
+                                             conditionalPanel(
+                                                     condition = "input.yVariable == student_id",
+                                                     radioButtons(inputId = "arrage",
+                                                                  label = "Arrage By:",
+                                                                  choices = list("Student ID" = "id",
+                                                                                 "Frequency" = "freq"),
+                                                                  selected = "id")
+                                             ),
+                                             tags$img(src = "https://40.media.tumblr.com/3d01ef923a34a3546133a12833e5aae2/tumblr_np0zi0MOM31uwjn2uo1_540.png", width = "550px", height = "600px")
+                                      )
+                             ),
+                             # Histogram of Referrals per student
+                             tabPanel("Arrange Count", 
+                                      column(12,
+                                             plotOutput("plot1", height = "500px"),
+                                             radioButtons("referralCount", label = "Arrange By:",
+                                                          choices = list("Student ID" = "id", "Frequency" = "freq"),
+                                                          selected = "id"
                                              )
-                                )
-                        )
-#                 ),
-#                 tabPanel("ggplot2",  plotOutput("ggvis1")),
-#                 )
-        )
+                                      )
+                             ),
+                             # histogram of Referrals per hour AND per month
+                             tabPanel("Time",
+                                      column(12,
+                                             plotOutput("time", height = "500px"),
+                                             radioButtons("timeType", label = "",
+                                                          choices = list("Hours of the Day" = "H",
+                                                                         "Day of the Week" = "A",
+                                                                         "Month of the Year" = "m")
+                                             )
+                                      )
+                             ),
+                             tabPanel("Interactive Example",
+                                      column(12,
+                                             showOutput("myChart", "polycharts")
+                                             )
+                                      )
+                             #                 ),
+                             #                 tabPanel("ggplot2",  plotOutput("ggvis1")),
+                             #                 )
+                 )
         )
 )
 
-ui <- dashboardPage(header, sidebar, body)
+ui <- dashboardPage(header, sidebar, body, skin = "yellow")
 
 server <- function(input, output) {
-        set.seed(122)
-        histdata <- rnorm(500)
         
+        # create ggFinal
+        #input_x <- reactive(input$xaxis)
+        
+        # SAMPLE
+        plotData <- reactive({
+                df <- data[,c("id",input$yVariable)]
+                names(df) <- c("id","x")
+                df
+        })
+        # define axis for labeling
+        label <- c("Student ID: ")
+        
+        
+        label <- reactive({
+                if(input$yVariable == "student_id") return("Student ID: ")
+                else if(input$yVariable == "grade") return("Grade: ")
+        })
+        
+        reactive({
+                if(input$yVariable == "student_id"){
+                plotData() %>%  ggvis(x=~x) %>%
+                        layer_histograms(width = 1, fill := "green") %>%
+                        #arrange(y) %>%
+                        add_axis("x", title = "Student ID") %>%
+                        add_tooltip(function(df){paste0("Student ID: ",
+                                                        df$xmin+0.5,
+                                                        "<br>",
+                                                        "Referrals: ",
+                                                        df$stack_upr_)}, "hover")
+        } else if(input$yVariable == "grade"){
+                plotData() %>%  ggvis(x=~x) %>%
+                        layer_histograms(width = 1, fill := "green") %>%
+                        #arrange(y) %>%
+                        add_axis("x", title = "Grade Level") %>%
+                        add_tooltip(function(df){paste0("Grade: ",
+                                                        df$xmin+0.5,
+                                                        "<br>",
+                                                        "Referrals: ",
+                                                        df$stack_upr_)}, "hover")}  
+        }) %>%  bind_shiny("ggvisPlot")
+
+
         output$plot1 <- renderPlot({
                 # create histogram of misbehavior counts by student_id
                if (input$referralCount == "id") {
@@ -144,29 +204,7 @@ server <- function(input, output) {
                             main = "<-- Most Offenses to Least Offenses -->")
                }
         })
-        
-        
-        
-        # create a reactive expression wrapper for input$size
-        #input_fill <- reactive(input$fill)
-        data %>% 
-                ggvis(~grade) %>% 
-                layer_histograms(width = 1) %>% 
-                add_tooltip(function(data){paste0("Grade: ", data$xmin + 0.5)}, "hover") %>%
-                bind_shiny("plotGrade")
 
-                # using layer_histograms() {ggvis}
-
-        
-        data %>% 
-                ggvis(~student_id) %>% 
-                layer_histograms(width = 1) %>% 
-                add_tooltip(function(data) {paste0("Student ID: ",
-                                                   data$xmin + 0.5,
-                                                   "<br>",
-                                                   "referrals: ", data$stack_upr_)}, "hover") %>% 
-                bind_shiny("ggvis1")
-        
         ## Creat Time Plot
         output$time <- renderPlot({
                 if(input$timeType == "H") {
@@ -175,13 +213,22 @@ server <- function(input, output) {
                              main = "Number of Referrals across time", nclass = 24)
                 } else if(input$timeType == "m") {
                         hist(as.numeric((format(data$date_time, format = "%m"))),
-                             ylab = "Number of Referrals", xlab = "Month",
+                             ylab = "Number of Referrals", xlab = "Month of the year",
                              main = "Number of Referrals across time", nclass = 12)
                 } else if(input$timeType == "A") {
                         hist(as.numeric((format(data$date_time, format = "%u"))),
-                             ylab = "Number of Referrals", xlab = "Month",
+                             ylab = "Number of Referrals", xlab = "Day of the Week",
                              main = "Number of Referrals per Day of the Week", nclass = 7)
                 }
+        })
+        
+        ## A Sample of Interactive Output Using rCharts
+        output$myChart <- renderChart({
+                names(iris) = gsub("\\.", "", names(iris))
+                p1 <- rPlot(input$x, input$y, data = iris, color = "Species", 
+                            facet = "Species", type = 'point')
+                p1$addParams(dom = 'myChart')
+                return(p1)
         })
 }
 
